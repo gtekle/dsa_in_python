fe435d8103169615779b212780e27e157945d7a9
tYohannes <teklegy@gmail.com>
2023-01-23 10:55:27 +0300

    Add another module import demo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from user import User
+from post import Post
 
 user_one = User("tek@mail.com", "Tekle", "password", "Software Architect")
 user_one.get_user_info()
@@ -6,4 +7,6 @@ user_one.get_user_info()
 user_two = User("john@mail.com", "John", "pass", "Senior Software Architect")
 user_two.get_user_info()
 
+first_post = Post("Hello, this is my first post", user_one.name)
+first_post.get_post_info()
 
