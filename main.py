297e0ea92158a6f777dd6dc52302cc5dceb404cf
tYohannes <teklegy@gmail.com>
2023-01-23 10:38:14 +0300

    Add import user module in main module as a demo

---
+++ main.py
@@ -0,0 +1,9 @@
+import user
+
+user_one = user.User("tek@mail.com", "Tekle", "password", "Software Architect")
+user_one.get_user_info()
+
+user_two = user.User("john@mail.com", "John", "pass", "Senior Software Architect")
+user_two.get_user_info()
+
+
