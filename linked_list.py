58b24a7132fe750160583cc60cf5e0c390744d46
tYohannes <teklegy@gmail.com>
2023-01-24 11:19:48 +0300

    Add insert_at_begining and print_ll methods

--- linked_list.py
+++ linked_list.py
@@ -8,6 +8,18 @@ class LinkedList:
   def __init__(self):
    self.head = None
 
+  def insert_at_begining(self, data):
+   self.head = Node(data, self.head)
+
+  def print_ll(self):
+   itr = self.head
+   while itr:
+     print(f"{itr.data} -->")
+     itr = itr.next
 
 
+ll = LinkedList()
+ll.insert_at_begining(34)
+ll.insert_at_begining(21)
+ll.print_ll()
 
