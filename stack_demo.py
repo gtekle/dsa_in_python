569fc7c8be87c8d0e8d849a97572c5b67b677cbe
tYohannes <teklegy@gmail.com>
2023-01-30 11:22:30 +0300

    Add stack impl using collections.deque

--- stack_demo.py
+++ stack_demo.py
@@ -1,8 +1,27 @@
 from collections import deque
-stack = deque()
-stack.append("https://www.cnn.com/")
-stack.append("https://www.cnn.com/world")
-stack.append("https://www.cnn.com/india")
-stack.append("https://www.cnn.com/china")
 
-print(stack.pop())
+class Stack:
+  def __init__(self):
+    self.container = deque()
+
+  def push(self, value):
+    self.container.append(value)
+
+  def pop(self):
+    return self.container.pop()
+
+  def peek(self):
+    return self.container[-1]
+
+  def is_empty(self):
+    return len(self.container) == 0
+
+  def size(self):
+    return len(self.container)
+
+
+s = Stack()
+s.push(5)
+#print(s.peek())
+#s.pop()
+
